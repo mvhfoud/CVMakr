4f9b4f35e3d39b4032b3d48f15a4a1b02015aeed
YOUSSEF MAHFOUD <151474933+mvhfoud@users.noreply.github.com>
2025-11-03 01:16:16 +0100

    cleanup

--- run_cvmakr.bat
+++ run_cvmakr.bat
@@ -1,9 +1,9 @@
 @echo off
 :: Set environment variables
-set "DRIVE_FOLDER_ID=1j0McbhmRarOzZkZYKEj9igGdh-SgOWGF"
-set "DRIVE_FOLDER_NAME=Resumes_CDI"
-set "SHEET_ID=1UovwuNHj9EyPyLcG1SZi-0u3xB3jZagE45OyaYun7g0"
-set "CLIENT_SECRETS_FILE=client_secret_236671734134-vrce2q6p4895haqvupssujhff5cmeq1l.apps.googleusercontent.com.json"
+set "DRIVE_FOLDER_ID=replace"
+set "DRIVE_FOLDER_NAME=replace"
+set "SHEET_ID=replace"
+set "CLIENT_SECRETS_FILE=replace"
 
 :: Run Streamlit app
 streamlit run main_cloud.py
